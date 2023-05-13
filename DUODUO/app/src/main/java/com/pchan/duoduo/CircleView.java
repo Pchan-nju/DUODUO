f31e15127c02a43c8b19afd04fbb32332a3e4518
Pchan-nju <122456738+Pchan-nju@users.noreply.github.com>
2023-05-13 16:53:49 +0800

    add CircleView.java

---
+++ DUODUO/app/src/main/java/com/pchan/duoduo/CircleView.java
@@ -0,0 +1,53 @@
+package com.pchan.duoduo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+/*绘制进度圈*/
+public class CircleView extends View {
+    private int circleColor = 0xFF0000;
+    private Paint.Style circleStyle = Paint.Style.FILL;
+    private float circleRadius = 1.0f;
+    private float circleCenterPositionX = 0.0f;
+    private float circleCenterPositionY = 0.0f;
+
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public void setCircleColor(int color) {
+        this.circleColor = color;
+    }
+
+    public void setCircleStyle(Paint.Style style) {
+        this.circleStyle = style;
+    }
+
+    public void setCircleRadius(float radius) {
+        this.circleRadius = radius;
+    }
+
+    public void setCircleCenterPosition(float x, float y) {
+        this.circleCenterPositionX = x;
+        this.circleCenterPositionY = y;
+    }
+
+    public void setCircle(int color, Paint.Style style, float radius, float x, float y) {
+        setCircleColor(color);
+        setCircleRadius(radius);
+        setCircleStyle(style);
+        setCircleCenterPosition(x, y);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(circleColor);
+        paint.setStyle(circleStyle);
+        paint.setAntiAlias(true); // 采用抗锯齿功能
+        canvas.drawCircle(circleCenterPositionX,circleCenterPositionY,circleRadius,paint);
+    }
+}
--- DUODUO/app/src/main/java/com/pchan/duoduo/MainActivity.java
+++ DUODUO/app/src/main/java/com/pchan/duoduo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pchan.duoduo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*获取屏幕中心坐标*/
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int centerX = displayMetrics.widthPixels / 2;
+        int centerY = displayMetrics.heightPixels / 2;
+
+        /*在屏幕中央绘制圆形*/
+        CircleView circleView = new CircleView(this);
+        circleView.setCircle(0xFFFFB6C1, Paint.Style.FILL, 400.0f, centerX,centerY);    // 设置圆形参数
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_main);
+        constraintLayout.addView(circleView);
     }
 }
