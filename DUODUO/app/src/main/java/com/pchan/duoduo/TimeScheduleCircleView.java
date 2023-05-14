9030a2746c9545e72f6ec628799f135766fad632
Pchan-nju <122456738+Pchan-nju@users.noreply.github.com>
2023-05-14 15:57:09 +0800

    add StartActivity & TimeScheduleCircleView.java

---
+++ DUODUO/app/src/main/java/com/pchan/duoduo/TimeScheduleCircleView.java
@@ -0,0 +1,10 @@
+package com.pchan.duoduo;
+
+import android.content.Context;
+
+/***根据已有的ProjectTimeSchedule确定展示***/
+public class TimeScheduleCircleView extends CircleView{
+    public TimeScheduleCircleView(Context context) {
+        super(context);
+    }
+}
